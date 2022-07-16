8123e468c985416d7e030100c0c1532fea4eec78
Joy Paul <joypaul3592@gmail.com>
2022-07-17 02:04:13 +0600

    done my job

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { FcInspection, FcManager } from "react-icons/fc";
+import React, { useState } from 'react';
+import { FcInspection, FcDeleteDatabase } from "react-icons/fc";
 import { TbApiApp } from "react-icons/tb";
+import { TbArrowForward } from "react-icons/tb";
+import { RiDeleteBin4Line } from "react-icons/ri";
 import { SiCheckio } from "react-icons/si";
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { addTodo, deleteTodo, removeTodo } from '../actions';
+import { CgPlayListRemove } from "react-icons/cg";
 
 const Todo = () => {
 
@@ -11,15 +14,19 @@ const Todo = () => {
     const d = new Date();
     const dayName = days[d.getDay()];
 
-
+    // const [inputVlue, setinputValue] = useState('')
+    const list = useSelector((state) => state.todoReducer.list)
     const dispatch = useDispatch()
 
+
+
+    console.log(list);
     const inputOnSubmit = e => {
         e.preventDefault()
 
         const inputData = e.target.myInput.value
         dispatch(addTodo(inputData))
-
+        // setinputValue(e.target.myInput.value)
 
         e.target.myInput.value = ''
     }
@@ -38,6 +45,31 @@ const Todo = () => {
 
                 </form>
             </div>
+
+            <div >
+                {
+                    list.map((todo) => {
+                        return (
+                            <div key={todo.id} className=' mt-6  flex items-center justify-between w-2/4 mx-auto shadow-md border-[1px] px-2'>
+                                <div className='w-11/12 h-full  flex items-center py-1 '>
+                                    <TbArrowForward className=' text-xl mr-4 text-green-800' />
+                                    <h1 className=' font-semibold text-md text-gray-700'>{todo.data}</h1>
+                                </div>
+                                <button
+                                    onClick={() => dispatch(deleteTodo(todo.id))}
+                                    className='py-[5px] text-red-700 w-1/12 '><RiDeleteBin4Line className=' text-xl  mx-auto' /></button>
+                            </div>
+                        )
+                    })
+                }
+
+
+            </div>
+
+            <dir className='mt-8  w-2/4 mx-auto flex justify-end'>
+                <button className='flex items-center bg-red-100 text-md px-4 rounded text-gray-700 ' onClick={() => dispatch(removeTodo())}>Clear All <CgPlayListRemove className=' ml-2 text-xl mt-1' /></button>
+            </dir>
+
         </div>
     );
 };
--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -3,6 +3,40 @@ const initialList = {
 }
 
 
-const todoReducers = (state = initialList, action) => {
+const todoReducer = (state = initialList, action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
 
+            const { id, data } = action.payload;
+
+            return {
+                ...state,
+                list: [
+                    ...state.list,
+                    {
+                        id: id,
+                        data: data
+                    }
+                ]
+            }
+
+        case 'DELETE_TODO':
+
+            const newList = state.list.filter((todo) => todo.id !== action.id)
+            return {
+                ...state,
+                list: newList
+            }
+
+        case 'REMOVE_TODO':
+
+            return {
+                ...state,
+                list: []
+            }
+
+        default: return state
+    }
 }
+
+export default todoReducer;
